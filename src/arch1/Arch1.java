3f5c9e7f1735f2cbadf9a5adb0e42b9fd4b8f049
Emy Canahuati <emy_canahuati@unitec.edu>
2026-01-22 23:44:19 -0600

    Ultimo hoy si

--- src/arch1/Arch1.java
+++ src/arch1/Arch1.java
@@ -16,7 +16,15 @@ public class Arch1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("tengo sueñooo");
+<<<<<<< HEAD
+        System.out.println("hola mundo");
+        System.out.println("Me ira bien en programacion II");
+=======
+        System.out.println("chao mundo");
+        for (int i=0;i<4;i++){
+            System.out.println(i);
+        }
+>>>>>>> rama
     }
     
 }
