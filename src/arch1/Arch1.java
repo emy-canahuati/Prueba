940ced9d7f5ef7bab8adc44a4214354f31a7725c
Emy Canahuati <emy_canahuati@unitec.edu>
2026-01-22 23:59:54 -0600

    t

--- src/arch1/Arch1.java
+++ src/arch1/Arch1.java
@@ -17,6 +17,7 @@ public class Arch1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("tengo suenooo");
+        System.out.println("me matare");
     }
     
 }
